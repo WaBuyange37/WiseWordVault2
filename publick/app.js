0c7fe367e05bb02a8b34d6b9e53269d7846a2f89
WaBuyange37 <nzelabeatblack37@gmail.com>
2025-04-24 23:55:50 +0200

    testing Dom first

--- publick/app.js
+++ publick/app.js
@@ -1,1 +1,15 @@
-// frontend Logic Fetch DOM
+let author = document.getElementById('AuthorName');
+let dateYaQuote = document.getElementById('date');
+let timeYaQuote = document.getElementById('time');
+let quote = document.getElementById('wise');
+function saveQuote(){
+  author=author.value;
+  timeYaQuote.value;
+  dateYaQuote.value;
+  quote.value;
+  let fullDate= new Date(`${dateYaQuote}T${timeYaQuote}`)
+  console.log(`${author} uyu niwe wanditse iy :
+    ${quote}
+    iki gihe: ${fullDate}`);
+  
+}
