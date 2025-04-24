4493e5ce17b87771f3e76e86139005489be07ce1
WaBuyange37 <nzelabeatblack37@gmail.com>
2025-04-25 00:21:35 +0200

    saving-- try to put them on array first

--- publick/app.js
+++ publick/app.js
@@ -1,15 +1,28 @@
+let quotes = []
 let author = document.getElementById('AuthorName');
 let dateYaQuote = document.getElementById('date');
 let timeYaQuote = document.getElementById('time');
 let quote = document.getElementById('wise');
 function saveQuote(){
   author=author.value;
-  timeYaQuote.value;
-  dateYaQuote.value;
-  quote.value;
+  timeYaQuote = timeYaQuote.value;
+  dateYaQuote = dateYaQuote.value;
+  quote = quote.value;
   let fullDate= new Date(`${dateYaQuote}T${timeYaQuote}`)
-  console.log(`${author} uyu niwe wanditse iy :
-    ${quote}
-    iki gihe: ${fullDate}`);
+  
+  const newQuote = {
+    author: author,
+    time: timeYaQuote,
+    date: dateYaQuote,
+    quote: quote,
+    // nibaYakunze(){
+    //   console.log(`${this.author} uyu niwe wanditse iy :
+    //   ${quote}
+    //   iki gihe: ${this.fullDate}`);
+    // }
+  }
+  quotes.push(newQuote);
+  console.log(quotes);
+  saveQuote()
   
 }
